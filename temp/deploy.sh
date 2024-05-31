33c1a282c443cad510910893a8a1b4d44a17d9bf
lihuanacct <lihuan0747@gmail.com>
2024-05-31 18:12:54 +0800

    Site updated: 2024-05-31 18:12:53

--- temp/deploy.sh
+++ temp/deploy.sh
@@ -1,18 +1,35 @@
 cd D:/Develop/workspace/javaworkspace/website/source/temp/
 echo -e "\033[47;30m>>>>>>>>>>>>>>>>>>>>获取文件名<<<<<<<<<<<<<<<<<<<<\033[0m"
+#!/bin/bash
 
+cd D:/Develop/workspace/javaworkspace/website/source/temp/
+#获取文件名
 filename=$(ls *.md)
+#去掉后缀
 filename_no_ext="${filename%.*}"
 
 echo -e "\033[47;30m>>>>>>>>>>>>>>>>>>>>开始写入front-matter<<<<<<<<<<<<<<<<<<<<\033[0m"
 
+#获取第一行的内容
+first_line1=$(sed -n '1p' $filename)
+#获取第二行的内容
+first_line2=$(sed -n '2p' $filename)
+
+#删除第一行、第二行的内容
+sed -i '1,2d' $filename
+
+#写入front-matter
 sed -i '1i\\n' $filename
 sed -i '1i\---' $filename
-sed -i '1i\categories: [测试]' $filename
-sed -i '1i\tags: [测试]' $filename
+sed -i '1i\categories: 测试' $filename
+sed -i '1i\tags: 测试' $filename
 sed -i '1i\date: ${date}' $filename
 sed -i "1i\\title: ${filename_no_ext}" "$filename"
 sed -i '1i\---' $filename
+
+#
+sed -i "4s/测试/${first_line1}/" $filename
+sed -i "5s/测试/${first_line2}/" $filename
 
 echo -e "\033[47;30m>>>>>>>>>>>>>>>>>>>>开始复制并删除原文件<<<<<<<<<<<<<<<<<<<<\033[0m"
 
